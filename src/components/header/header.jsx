4cb63a6639b647d71f78f5e762d115c914c5b977
Saumy Pandey <s8795p@gmail.com>
2021-10-20 01:43:11 +0530

    header size fixed

--- src/components/header/header.jsx
+++ src/components/header/header.jsx
@@ -40,8 +40,8 @@ class HeaderComponent extends Component {
         return (
           <div className="header-wrapper" style={{"height": this.state.height}}>
             <div className="header">
-                <h1>Saumy Pandey</h1>
-                <h3>Web Developer</h3>
+                <h3>Saumy Pandey</h3>
+                <h6>Web Developer</h6>
             </div>
             <div className="scroll-down-btn">
               <FloatingActionButton mini={true} backgroundColor="#A80202" onClick={this.onScrollToIntro.bind(this)}>
